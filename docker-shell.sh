70c37597e086640762156d9b5488a102c6ba96c8
Sydney Levy <levy.sydney@gmail.com>
2024-11-04 15:06:03 -0500

    dataset updates...

--- docker-shell.sh
+++ docker-shell.sh
@@ -4,9 +4,9 @@ set -e
 
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCS_BUCKET_NAME="cheese-app-data-versioning"
-export GCP_PROJECT="ac215-project"
-export GCP_ZONE="us-central1-a"
+export GCS_BUCKET_NAME="data-versioning-ac215-hw2"
+export GCP_PROJECT="ac215-testproject"
+export GCP_ZONE="us"
 export GOOGLE_APPLICATION_CREDENTIALS="/secrets/data-service-account.json"
 
 
